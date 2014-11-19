626f5ea1b6496a2a2c24b1b73553438995f9acd1
baltasarq <baltasarq@gmail.com>
2014-11-19 12:27:17 +0100

    For some reason, this file was not included.

---
+++ Ui/MenuComponentTreeNodes/RootMenuTreeNode.cs
@@ -0,0 +1,13 @@
+using System;
+
+using RAppMenu.Core.MenuComponents;
+
+namespace RAppMenu.Ui.MenuComponentTreeNodes {
+	public class RootMenuTreeNode: MenuTreeNode {
+		public RootMenuTreeNode(RootMenu rme)
+			:base( rme )
+		{
+		}
+	}
+}
+
