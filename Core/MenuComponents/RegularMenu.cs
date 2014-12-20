eb11250ddefa5bbd74015de3ebb153903090eab5
baltasarq <baltasarq@gmail.com>
2014-12-20 17:34:53 +0100

    RegularMenus now hold a List of PDF files... This is due to Issue #3

--- Core/MenuComponents/RegularMenu.cs
+++ Core/MenuComponents/RegularMenu.cs
@@ -1,11 +1,45 @@
 ﻿using System;
 
 namespace RAppMenu.Core.MenuComponents {
-    public class RegularMenu: Menu {
+    public partial class RegularMenu: Menu {
         public RegularMenu(string name, Menu parent)
             :base( name, parent )
         {
+            this.pdfList = new PDFList( this );
         }
+
+        public override void Add(MenuComponent mc)
+        {
+            base.Add( mc );
+
+            // We need to store the new PDF File in the list
+            if ( mc is PdfFile ) {
+                this.PDFNameList.Add( mc.Name );
+            }
+        }
+
+        /// <summary>
+        /// Gets the PDF list.
+        /// </summary>
+        /// <value>A <see cref="PDFList"/>.</value>
+        public PDFList PDFNameList {
+            get {
+                return this.pdfList;
+            }
+        }
+
+        public override string ToString()
+        {
+            string toret = base.ToString();
+
+            toret = "[RegularMenu " + toret;
+            toret = " " + this.PDFNameList.ToString();
+
+            return toret;
+        }
+                
+
+        private PDFList pdfList;
     }
 }
 
---
+++ Core/MenuComponents/RegularMenuPDFList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace RAppMenu.Core.MenuComponents {
+    public partial class RegularMenu {
+        /// <summary>
+        /// Represents the collection of PDF's in this regular menu.
+        /// </summary>
+        public class PDFList: Collection<string> {
+            public PDFList(RegularMenu owner)
+            {
+                this.owner = owner;
+            }
+
+            /// <summary>
+            /// Gets the owner of this list of PDF's
+            /// </summary>
+            /// <value>The owner, a <see cref="RegularMenu"/>.</value>
+            public RegularMenu Owner {
+                get {
+                    return this.owner;
+                }
+            }
+
+            public override string ToString()
+            {
+                var toret = new StringBuilder();
+
+                toret.Append( "[PDFList PDFs=[" );
+
+                foreach(string pdfName in this) {
+                    toret.Append( pdfName + ' ' );
+                }
+
+                return toret.Append( "]]" ).ToString();
+            }
+
+            private RegularMenu owner;
+        }
+}
+}
+
