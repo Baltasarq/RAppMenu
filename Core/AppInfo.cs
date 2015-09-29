02db41a354d50516e2769bbc9af63d39a6900235
baltasarq <baltasarq@gmail.com>
2015-09-29 15:03:22 +0200

    Updated version: 1.2.3

--- Core/AppInfo.cs
+++ Core/AppInfo.cs
@@ -7,7 +7,7 @@ namespace RWABuilder.Core {
 		public const string Name = "RWABuilder";
 		public const string Web = "http://www.ipez.es/rwizard/";
 		public const string Help = "http://github.com/Baltasarq/RWABuilder/wiki/User-Manual";
-        public const string Version = "1.2.2 20150922";
+        public const string Version = "1.2.3 20150929";
         public const string FileExtension = "xml";
 		public const string AppsExtension = "rwa";
         public const string LogFile = Name + ".errors.log";
