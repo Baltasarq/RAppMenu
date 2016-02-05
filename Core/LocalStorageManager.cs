67fa83a46645f3be02d389a4c9a9ffef99d1f9fa
baltasarq <baltasarq@gmail.com>
2016-02-05 22:06:46 +0100

    Added "import" option

---
+++ Core/LocalStorageManager.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace RWABuilder.Core {
+	/// <summary>
+	/// Local storage manager.
+	/// This class provides the management of configuration files,
+	/// temporary files, and other configuration needs in general.
+	/// </summary>
+	public static class LocalStorageManager {
+		public const string LogFile = AppInfo.Name + ".errors.log";
+
+		public const string RegistryPath = "Software\\RWizard";
+		public const string RegistryVersionKey = "Version";
+		public const string RegistryInstallPathKey = "InstallPath";
+
+		/// <summary>
+		/// Gets the application configuration folder ready to work.
+		/// </summary>
+		/// <returns>The app config folder, as a string.</returns>
+		public static string PrepareAppConfigFolder()
+		{
+			string toret = Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData );
+			toret = Path.Combine( toret, AppInfo.Name );
+
+			Directory.CreateDirectory( toret );
+			return toret;
+		}
+
+		/// <summary>
+		/// Returns the full path to an unexisting temporary directory.
+		/// </summary>
+		/// <returns>The full path to the temp folder, as a string.</returns>
+		public static string CreateTempFolder()
+		{
+			string toret;
+
+			Trace.WriteLine( DateTime.Now + ": Creating temporary folder"  );
+
+			do {
+				toret = Path.Combine( Path.GetTempPath(), Path.GetRandomFileName() );
+			} while( File.Exists( toret )
+				  || Directory.Exists( toret ) );
+
+			Trace.WriteLine( DateTime.Now + ": Created temporary folder: " + toret  );
+			return toret;
+		}
+
+		/// <summary>
+		/// Gets the default applications folder.
+		/// </summary>
+		/// <value>The default applications folder, as a string.</value>
+		public static string DefaultApplicationsFolder {
+			get {
+				GetMainAppInfo();
+				return pathToApplications = Path.Combine( PathToMainApp, "ApplicationsV2" );
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the applications folder.
+		/// By default, it is "applications".
+		/// </summary>
+		/// <value>The applications folder, as a string.</value>
+		public static string ApplicationsFolder {
+			get {
+				if ( string.IsNullOrWhiteSpace( pathToApplications ) ) {
+					pathToApplications = DefaultApplicationsFolder;
+				}
+
+				return pathToApplications;
+			}
+			set {
+				pathToApplications = ( value ?? "" ).Trim();
+			}
+		}
+
+		/// <summary>
+		/// Gets the default applications folder.
+		/// </summary>
+		/// <value>The default applications folder, as a string.</value>
+		public static string DefaultPdfFolder {
+			get {
+				GetMainAppInfo();
+				return Path.Combine( PathToMainApp, "Pdf" );
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the pdf folder.
+		/// </summary>
+		/// <value>The pdf folder path, as a string.</value>
+		public static string PdfFolder {
+			get {
+				if ( string.IsNullOrWhiteSpace( pathToPDFs ) ) {
+					pathToPDFs = DefaultPdfFolder;
+				}
+
+				return pathToPDFs;
+			}
+			set {
+				pathToPDFs = ( value ?? "" ).Trim();
+			}
+		}
+
+		public static string DefaultGraphsFolder {
+			get {
+				GetMainAppInfo();
+				return Path.Combine( PathToMainApp, "Graphs" );
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the graphs folder.
+		/// </summary>
+		/// <value>The graphs folder, as a string.</value>
+		public static string GraphsFolder {
+			get {
+				if ( string.IsNullOrWhiteSpace( pathToGraphics ) ) {
+					pathToGraphics = DefaultGraphsFolder;
+				}
+
+				return pathToGraphics;
+			}
+			set {
+				pathToGraphics = ( value ?? "" ).Trim();
+			}
+		}
+
+		/// <summary>
+		/// Gets the path to main app.
+		/// </summary>
+		/// <value>The path to main app, as a string.</value>
+		public static string PathToMainApp {
+			get {
+				return pathToMainApp;
+			}
+		}
+
+		/// <summary>
+		/// Gets the main app version info.
+		/// </summary>
+		/// <value>The main app version, as a string.</value>
+		public static string MainAppVersionInfo {
+			get {
+				return mainAppVersion;
+			}
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether the main app was located.
+		/// </summary>
+		/// <value><c>true</c> if main app located; otherwise, <c>false</c>.</value>
+		public static Boolean MainAppLocated {
+			get {
+				return mainAppLocated;
+			}
+		}
+
+		/// <summary>
+		/// Prepares the log for all events in the app.
+		/// </summary>
+		public static void BuildLog()
+		{
+			string logPath = Path.Combine(
+				PrepareAppConfigFolder(),
+				LogFile );
+
+			Trace.Listeners.Add( new TextWriterTraceListener(
+				new FileStream( logPath, FileMode.Create )
+			) );
+
+			Trace.AutoFlush = true;
+
+			Trace.Write( AppInfo.Name );
+			Trace.Write( ' ' );
+			Trace.Write( AppInfo.Version );
+			Trace.Write( ' ' );
+			Trace.WriteLine( DateTime.Now );
+			Trace.WriteLine( "================================================" );
+		}
+
+		/// <summary>
+		/// Closes the log, avoiding any data loss.
+		/// </summary>
+		public static void CloseLog()
+		{
+			Trace.WriteLine( "================================================" );
+			Trace.WriteLine( DateTime.Now + ": finishing...." );
+
+			Trace.Flush();
+			Trace.Close();
+		}
+
+		/// <summary>
+		/// Gets the main app info, suing the registry.
+		/// </summary>
+		/// <returns>The main app info.</returns>
+		public static void GetMainAppInfo()
+		// Root: "HKLM"; Subkey: "Software\RWizard"; ValueType: string; ValueName: "InstallPath";
+		// Root: "HKLM"; Subkey: "Software\RWizard"; ValueType: string; ValueName: "Version";
+		{
+			Trace.WriteLine( DateTime.Now + ": " + "Starting registry search..." );
+
+			try {
+				var regKey = Microsoft.Win32.Registry.LocalMachine.OpenSubKey( RegistryPath );
+
+				if ( regKey != null ) {
+					pathToMainApp = (string) regKey.GetValue( RegistryInstallPathKey );
+					mainAppVersion = (string) regKey.GetValue( RegistryVersionKey );
+					mainAppLocated = true;
+
+					Trace.WriteLine( DateTime.Now + ": " + "main app "
+						+ mainAppVersion + " located at: " + pathToMainApp );
+				} else {
+					Trace.WriteLine( DateTime.Now + ": " + "main app not installed." );
+				}
+			} catch(Exception exc)
+			{
+				Trace.WriteLine( DateTime.Now + ": " + "Error trying to locate main app:" );
+				Trace.WriteLine( exc.Message + "\n" + exc.StackTrace );
+			}
+
+			Trace.WriteLine( DateTime.Now + ": " + "Finished registry search." );
+		}
+
+		private static string mainAppVersion = "";
+		private static string pathToMainApp = ".";
+		private static bool mainAppLocated = false;
+		private static string pathToApplications = "";
+		private static string pathToPDFs = "";
+		private static string pathToGraphics = "";
+	}
+}
+
---
+++ Core/Package.cs
@@ -0,0 +1,487 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Collections.Generic; 
+
+
+namespace RWABuilder.Core {
+	/// <summary>
+	/// This is the packager, the class responsible of managing
+	/// Zip files which include the menu design and the resources,
+	/// i.e., PDF's and images.
+	/// </summary>
+	public class Package {
+		// Directories inside the zip
+		public const string ZipPdfDir = "Pdf/";
+		public const string ZipGrfDir = "Graph/";
+		public const string ZipAppsDir = "Applications/";
+		public const string ZipSrcDir = "Src/";
+		public const string ZipWinBinDir = "WinBin/";
+
+		// Labels for manifest file
+		public const string ManifestFileName = "manifest";
+		public const string EtqName = "Name";
+		public const string EtqApp = "App";
+		public const string EtqTime = "Time";
+		public const string EtqUUID = "UUID";
+		public const string EtqPDF = "Pdf";
+		public const string EtqGRF = "Grf";
+		public const string EtqWinBin = "WinBin";
+		public const string EtqSrc = "Src";
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="RWABuilder.Core.Packager"/> class.
+		/// </summary>
+		/// <param name="m">A MenuDesign object.</param>
+		public Package(MenuDesign m)
+			: this()
+		{
+			this.Menu = m;
+		}
+
+		public Package()
+		{
+			this.Menu = new MenuDesign();
+			this.grfFiles = new List<string>();
+			this.pdfFiles = new List<string>();
+			this.UUID = Guid.NewGuid();
+			this.PathToDir = LocalStorageManager.CreateTempFolder();
+		}
+
+		/// <summary>
+		/// Package this rwapp.
+		/// </summary>
+		public void Pack(string nf)
+		{
+			Trace.WriteLine( DateTime.Now + ": Packaging: " + nf );
+			Trace.Indent();
+
+			// Create resource lists
+			this.pdfFiles.Clear();
+			this.grfFiles.Clear();
+			this.pdfFiles.AddRange( this.Menu.GetPDFNameList() );
+			this.grfFiles.AddRange( this.Menu.GetGRFNameList() );
+
+			try {
+				using ( FileStream f = new FileStream( nf, FileMode.Create ) ) {
+					using( var zip = new ZipArchive( f, ZipArchiveMode.Create, true, Encoding.UTF8 ) )
+					{
+						if ( this.Menu.SourceCodePath.Length > 0 ) {
+							zip.CreateEntryFromFile( this.Menu.SourceCodePath,
+													  ZipSrcDir + Path.GetFileName( this.Menu.SourceCodePath ) );
+						}
+
+						if ( this.Menu.WindowsBinariesPath.Length > 0 ) {
+							zip.CreateEntryFromFile( this.Menu.WindowsBinariesPath,
+								ZipWinBinDir + Path.GetFileName( this.Menu.WindowsBinariesPath ) );
+						}
+
+						// Insert each pdf file in the zip
+						DumpResFilesToZip( zip, ZipPdfDir, this.pdfFiles );
+
+						// Insert each graphic file in the zip
+						DumpResFilesToZip( zip, ZipGrfDir, this.grfFiles );
+
+						// Insert fixed assets
+						DumpMenuFileToZip( this.Menu, zip );
+						DumpManifestToZip( this, zip );
+					}
+				}
+			} catch(ArgumentException exc) {
+				Trace.WriteLine( DateTime.Now + ": ERROR: locating file: " + exc.Message );
+				throw new IOException( "locating file: " + exc.Message );
+			} catch(FileNotFoundException exc) {
+				Trace.WriteLine( DateTime.Now + ": ERROR: file not found: " + exc.FileName );
+				throw new IOException( "file not found: " + exc.FileName );
+			} catch(IOException exc) {
+				Trace.WriteLine( DateTime.Now + ": ERROR input/output: " + exc.Message );
+				throw;
+			}
+
+			Trace.WriteLine( DateTime.Now + ": Packaging finished for: " + Menu.Name );
+			Trace.Unindent();
+			return;
+		}
+
+		/// <summary>
+		/// Creates and inserts the manifest into the zip file.
+		/// </summary>
+		/// <param name="zip">The ZipArchive object.</param>
+		private static void DumpManifestToZip(Package package, ZipArchive zip)
+		{
+			var manifestFile = zip.CreateEntry( ManifestFileName );
+
+			using (var entryStream = manifestFile.Open()) {
+				using (var streamWriter = new StreamWriter( entryStream )) {
+					streamWriter.WriteLine( "# " + AppInfo.Name );
+					streamWriter.WriteLine( "# RWizard packaged app" );
+					streamWriter.WriteLine( EtqName + ": " + package.Menu.Name );
+					streamWriter.WriteLine( EtqApp + ": " + ZipAppsDir + package.Menu.Name + "." + AppInfo.FileExtension );
+					streamWriter.WriteLine( EtqUUID + ": " + package.UUID );
+					streamWriter.WriteLine( EtqTime + ": " + DateTime.Now.ToString( @"yyyy-MM-dd\THH:mm:sszzz" ) );
+
+					if ( package.Menu.SourceCodePath.Length > 0 ) {
+						streamWriter.WriteLine( EtqSrc + ": " + ZipSrcDir + Path.GetFileName( package.Menu.SourceCodePath ) );
+					}
+
+					if ( package.Menu.WindowsBinariesPath.Length > 0 ) {
+						streamWriter.WriteLine( EtqWinBin + ": " + ZipWinBinDir + Path.GetFileName( package.Menu.WindowsBinariesPath ) );
+					}
+
+					foreach ( string file in package.pdfFiles ) {
+						streamWriter.WriteLine( EtqPDF + ": " + ZipPdfDir + Path.GetFileName( file ) );	
+					}
+
+					foreach ( string file in package.grfFiles ) {
+						streamWriter.WriteLine( EtqGRF + ": " + ZipGrfDir + Path.GetFileName( file ) );	
+					}
+
+					streamWriter.Flush();
+				}
+			}
+
+			return;
+		}
+
+		/// <summary>
+		/// Inserts the menu file.
+		/// This copies the XML of the design menu into the zip.
+		/// </summary>
+		/// <param name="zip">The ZipArchive object.</param>
+		private static void DumpMenuFileToZip(MenuDesign menu, ZipArchive zip)
+		{
+			var menuFile = zip.CreateEntry( ZipAppsDir + menu.Name + "." + AppInfo.FileExtension );
+
+			using (var entryStream = menuFile.Open()) {
+				using (var streamWriter = new StreamWriter( entryStream )) {
+					var settings = new XmlWriterSettings() {
+						Encoding = Encoding.UTF8,
+						Indent = true
+					};
+					var xmlDocWriter = XmlWriter.Create( streamWriter, settings );
+
+					// Write the document's XML
+					xmlDocWriter.WriteStartDocument();
+					menu.ToXml( xmlDocWriter );
+					xmlDocWriter.WriteEndDocument();
+					xmlDocWriter.Close();
+					xmlDocWriter.Dispose();
+				}
+			}
+
+			return;
+		}
+
+		/// <summary>
+		/// Inserts various files.
+		/// </summary>
+		/// <param name="zip">The ZipArchive object</param>
+		/// <param name="targetDir">The target dir.</param>
+		/// <param name="fileNames">The file names, as a IList<string> collection.</param>
+		private static void DumpResFilesToZip(ZipArchive zip, string targetDir, IList<string> fileNames)
+		{
+			for (int i = 0; i < fileNames.Count; ++i) {
+				string fileName = fileNames[ i ];
+
+				if ( File.Exists( fileName ) ) {
+					Trace.WriteLine(
+						String.Format(
+							"{0}: Inserting file '{1}' in zip at {2}",
+							DateTime.Now, fileName, targetDir
+						) );
+					
+					zip.CreateEntryFromFile( fileName, targetDir + Path.GetFileName( fileName ) );
+				} else {
+					fileNames.RemoveAt( i );
+					--i;
+					Trace.WriteLine(
+						String.Format(
+							"{0}: Warning, missing file '{1}'",
+							DateTime.Now, fileName
+						) );
+				}
+			}
+		}
+
+		/// <summary>
+		/// Unpackages the specified rwa file.
+		/// </summary>
+		/// <param name="pkgFile">The path to the rwa file, as a string.</param>
+		/// <returns>A packager with all the info, or null if unable to unpackage</returns>
+		public static Package Unpack(string pkgFile)
+		{
+			Package toret = null;
+			string destDir = "";
+			Trace.WriteLine( DateTime.Now + ": Start unpacking of: " + pkgFile );
+			Trace.Indent();
+
+			try {
+				destDir = LocalStorageManager.CreateTempFolder();
+				ZipFile.ExtractToDirectory( pkgFile, destDir );
+
+				toret = BuildFromDir( destDir );
+			} catch(IOException exc) {
+				string msg = "I/O error unpackaging: " + pkgFile + ":\n" + exc.Message;
+				Trace.WriteLine( DateTime.Now + ": " + msg );
+				throw new IOException( msg );
+			} catch(UnauthorizedAccessException exc) {
+				string msg = "no access possible to: " + pkgFile + ": " + exc.Message;
+				Trace.WriteLine( DateTime.Now + ": " + msg );
+				throw new IOException( msg );
+			} catch(NotSupportedException exc) {
+				string msg = "invalid format: " + exc.Message;
+				Trace.WriteLine( DateTime.Now + ": " + msg );
+				throw new IOException( msg );
+			} catch(InvalidDataException exc) {
+				string msg = "invalid data in: " + pkgFile + ": " + exc.Message;
+				Trace.WriteLine( DateTime.Now + ": " + msg );
+				throw new IOException( msg );
+			}
+
+			Trace.Unindent();
+			Trace.WriteLine( 
+				string.Format( "{0}: Finished unpacking of '{1}' to '{2}'",
+					DateTime.Now, pkgFile, destDir ) );
+			return toret;
+		}
+
+		/// <summary>
+		/// Creates a new instance of the <see cref="RWABuilder.Core.Package"/> class.
+		/// Specifically, it creates a package from a directory which sould have all the files
+		/// and subdirectories.
+		/// </summary>
+		/// <param name="destDir">The dir the package was unpacked.</param>
+		public static Package BuildFromDir(string dir) {
+			var toret = new Package();
+
+			toret.PathToDir = dir;
+			ParseManifestFor( dir, toret );
+			toret.CheckSanity();
+
+			return toret;
+		}
+
+		/// <summary>
+		/// Checks the sanity of the package.
+		/// The should be a manifest, all files in the manifest should exist...
+		/// </summary>
+		private void CheckSanity()
+		{
+			// Check there is an application menu
+			if ( this.Menu == null
+			  || this.Menu.Root == null )
+			{
+				throw new ArgumentException( "Empty menu design" );
+			}
+
+			// Check there is a name
+			if ( this.Menu.Name.Length == 0 ) {
+				this.Menu.Name = this.Menu.Root.Name;
+			}
+
+			// Check there is a valid UUID
+			if ( this.UUID == Guid.Empty ) {
+				throw new ArgumentException( "Package without valid UUID" );
+			}
+
+			// Check that all files exist
+			foreach(string path in this.ResourceFiles) {
+				if ( !File.Exists( path ) ) {
+					throw new IOException( "Path not found: " + path );
+				}
+			}
+
+			return;
+		}
+
+		/// <summary>
+		/// Parses the manifest for the given package.
+		/// </summary>
+		/// <param name="package">The package, as a Package object.</param>
+		private static void ParseManifestFor(string dir, Package package)
+		{
+			// Read file
+			using( var manifestFile = new StreamReader( Path.Combine( dir, ManifestFileName ) ) ) {
+				string line = manifestFile.ReadLine();
+				string[] parts;
+				string path;
+
+				while( line != null ) {
+					line = line.Trim();
+
+					if ( line[ 0 ] != '#' ) {
+						// The application itself (menu design in xml)
+						if ( line.ToUpper().StartsWith( EtqApp.ToUpper() ) ) {
+							parts = line.Split( new char[]{ ':' } );
+							path = Path.Combine( dir, parts[ 1 ].Trim() );
+
+							if ( parts.Length == 2 ) {
+								package.Menu = MenuDesign.LoadFromFile( path );
+							} else {
+								throw new ArgumentException( "parsing manifest: application entry erroneous" );
+							}
+						}
+						else
+						// The name of the package
+						if ( line.ToUpper().StartsWith( EtqName.ToUpper() ) ) {
+							parts = line.Split( new char[]{ ':' } );
+							
+							if ( parts.Length == 2 ) {
+								package.Menu.Name = parts[ 1 ].Trim();
+							} else {
+								throw new ArgumentException( "parsing manifest: name entry erroneous" );
+							}
+						}
+						else
+						// The UUID of the package
+						if ( line.ToUpper().StartsWith( EtqUUID.ToUpper() ) ) {
+							parts = line.Split( new char[]{ ':' } );
+
+							if ( parts.Length == 2 ) {
+								package.UUID = Guid.Parse( parts[ 1 ].Trim() );
+							} else {
+								throw new ArgumentException( "parsing manifest: uuid entry erroneous" );
+							}
+						}
+						else
+						// A PDF file
+						if ( line.ToUpper().StartsWith( EtqPDF.ToUpper() ) ) {
+							parts = line.Split( new char[]{ ':' } );
+							path = Path.Combine( dir, parts[ 1 ].Trim() );
+
+							if ( parts.Length == 2 ) {
+								package.AddPdfFile( path );
+							} else {
+								throw new ArgumentException( "parsing manifest: pdf entry erroneous" );
+							}
+						}
+						else
+						// A GRF file
+						if ( line.ToUpper().StartsWith( EtqGRF.ToUpper() ) ) {
+							parts = line.Split( new char[]{ ':' } );
+							path = Path.Combine( dir, parts[ 1 ].Trim() );
+
+							if ( parts.Length == 2 ) {
+								package.AddGrfFile( path );
+							} else {
+								throw new ArgumentException( "parsing manifest: grf entry erroneous" );
+							}
+						}
+						else
+						// A source targ.gz file
+						if ( line.ToUpper().StartsWith( EtqSrc.ToUpper() ) ) {
+							parts = line.Split( new char[]{ ':' } );
+
+							if ( parts.Length == 2 ) {
+								package.Menu.SourceCodePath = Path.Combine( dir, parts[ 1 ].Trim() );
+							} else {
+								throw new ArgumentException( "parsing manifest: src entry erroneous" );
+							}
+						}
+						else
+						// A windows binary file
+						if ( line.ToUpper().StartsWith( EtqWinBin.ToUpper() ) ) {
+							parts = line.Split( new char[]{ ':' } );
+
+							if ( parts.Length == 2 ) {
+								package.Menu.WindowsBinariesPath = Path.Combine( dir, parts[ 1 ].Trim() );
+							} else {
+								throw new ArgumentException( "parsing manifest: win-bin entry erroneous" );
+							}
+						}
+					}
+
+					line = manifestFile.ReadLine();
+				}
+			}
+
+			return;
+		}
+
+		/// <summary>
+		/// Adds a given pdf file.
+		/// </summary>
+		/// <param name="path">The path to the pdf file, as a string.</param>
+		public void AddPdfFile(string path) {
+			this.pdfFiles.Add( path );
+		}
+
+		/// <summary>
+		/// Adds a given graphic file.
+		/// </summary>
+		/// <param name="path">The path to the graphic file, as a string.</param>
+		public void AddGrfFile(string path) {
+			this.grfFiles.Add( path );
+		}
+
+		/// <summary>
+		/// Gets the UUI associated with this package.
+		/// </summary>
+		/// <value>The UUID, as a string.</value>
+		public Guid UUID {
+			get; private set;
+		}
+
+		/// <summary>
+		/// Gets the resource files that were used last time.
+		/// Does only have items after a call to Package() or Unpackage()
+		/// </summary>
+		/// <value>The resource files, as an array.</value>
+        public string[] ResourceFiles {
+			get {
+				List<string> toret = new List<string>();
+
+				toret.AddRange( this.pdfFiles );
+				toret.AddRange( this.grfFiles );
+				return toret.ToArray();
+			}
+		}
+
+		/// <summary>
+		/// Gets the pdf files that were inserted in the zip.
+		/// Does only have items after a call to Package() or Unpackage()
+		/// </summary>
+		/// <value>The pdf files, as an array of string.</value>
+		public string[] PdfFiles {
+			get {
+				return this.pdfFiles.ToArray();
+			}
+		}
+
+		/// <summary>
+		/// Gets the grf files that were inserted in the zip.
+		/// Does only have items after a call to Package()
+		/// </summary>
+		/// <value>The grf files, as an array of string.</value>
+		public string[] GrfFiles {
+			get {
+				return this.grfFiles.ToArray();
+			}
+		}
+
+		/// <summary>
+		/// Gets the design menu to be saved, or recently unpackaged.
+		/// </summary>
+		/// <value>The MenuDesign object.</value>
+		public MenuDesign Menu {
+			get; private set;
+		}
+
+		/// <summary>
+		/// It holds the directory
+		/// the files of this package are stored.
+		/// A directory is assigned from the beginning, but it just have files
+		/// stored after Unpack().
+		/// </summary>
+		/// <value>The path to dir, as a string.</value>
+		public string PathToDir {
+			get; private set;
+		}
+
+		private List<string> pdfFiles;
+		private List<string> grfFiles;
+	}
+}
